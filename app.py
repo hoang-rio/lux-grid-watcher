54c67387e765635a1f3d9eb54510326171356699
Hoàng Rio <donghoang.nguyen@outlook.com>
2024-04-12 17:11:33 +0700

    Play audio on chromecast device when lost grid or has grid

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
+import pychromecast
 import requests
 from config import BASE_URL, ACCOUNT, PASSWORD, SLEEP_TIME, USER_AGENT, SERIAL_NUM
 import config
 import logging
 import time
+from os import path
 
 logger = logging.getLogger(__file__)
 logging.basicConfig(
@@ -15,7 +17,23 @@ logging.basicConfig(
 )
 
 
-def get_run_time_data(session: requests.Session):
+def play_audio(audio_file: str):
+    chromecast, _ = pychromecast.get_listed_chromecasts([config.DEVICE_NAME])
+    if len(chromecast) > 0:
+        cast = chromecast[0]
+        logger.info("Cast info: %s", cast.cast_info)
+        cast.wait()
+        logger.info("Cast status: %s", cast.status)
+        mediaController = cast.media_controller
+        mediaController.play_media(
+            f"{config.AUDIO_BASE_URL}/{audio_file}", "audio/mp3")
+        mediaController.block_until_active()
+        logger.info("MediaControler status: %s", mediaController.status)
+    else:
+        logger.info("No device to play audio")
+
+
+def get_run_time_data(session: requests.Session, retry_count=0):
     try:
         logger.info("Start get runtime data")
         req = session.post(
@@ -38,29 +56,47 @@ def get_run_time_data(session: requests.Session):
                 int(res_json['fac']) / 100,
                 int(res_json['vacr']) / 10,
             )
+        last_grid_connected = True
+        if path.exists(config.STATE_FILE):
+            with open(config.STATE_FILE, 'r') as f:
+                last_grid_connected = f.read() == "True"
+        if last_grid_connected != is_grid_connected:
+            play_audio("has-grid.mp3" if is_grid_connected else "lost-grid.mp3")
+            with open(config.STATE_FILE, "w") as fw:
+                fw.write(str(is_grid_connected))
+        else:
+            logger.info("State did not change. Skip play notify auto")
         logger.info("Finish get runtime data")
     except Exception as e:
         logger.exception("Got exception when get run time data %s", e)
+        if retry_count < config.MAX_RETRY_COUNT:
+            login(session)
+            get_run_time_data(session, retry_count + 1)
+
+
+def login(session: requests.Session):
+    logger.info("Start login")
+    login_req = session.post(
+        f"{BASE_URL}/web/login",
+        data={"account": ACCOUNT, "password": PASSWORD},
+        headers={
+            "Referer": f"{BASE_URL}/web/login",
+            "UserAgent": USER_AGENT
+        }
+    )
+    logger.info("Login status code: %s", login_req.status_code)
 
 
 def main():
     session: requests.Session = requests.Session()
     try:
-        logger.info("Start login")
-        login_req = session.post(
-            f"{BASE_URL}/web/login",
-            data={"account": ACCOUNT, "password": PASSWORD},
-            headers={
-                "Referer": f"{BASE_URL}/web/login",
-                "UserAgent": USER_AGENT
-            }
-        )
-        logger.info("Login status code: %s", login_req.status_code)
+        login(session)
         while True:
             get_run_time_data(session)
             time.sleep(SLEEP_TIME)
     except Exception as e:
         logger.exception("Got error when run main %s", e)
+        exit(1)
 
 
 main()
--- config.py
+++ config.py
@@ -1,11 +1,18 @@
 import logging
+from dotenv import dotenv_values
 
-ACCOUNT = 'your account here'
-PASSWORD = 'your password here'
+env = dotenv_values(".env")
+
+ACCOUNT = env["ACCOUNT"]
+PASSWORD = env["PASSWORD"]
 BASE_URL = 'https://server.luxpowertek.com/WManage'
 SLEEP_TIME = 60
 USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:124.0) Gecko/20100101 Firefox/124.0"
-SERIAL_NUM = "Your inverter serial number"
+SERIAL_NUM = env["SERIAL_NUM"]
 LOG_FILE = 'app.log'
 LOG_LEVEL = logging.DEBUG
 LOG_FORMAT = "%(levelname)s:%(name)s: %(message)s"
+AUDIO_BASE_URL = env["AUDIO_BASE_URL"]
+STATE_FILE = 'grid_connect_state.ini'
+MAX_RETRY_COUNT = 3
+DEVICE_NAME = env["DEVICE_NAME"]
