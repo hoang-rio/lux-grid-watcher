4d1e156848023e4e8c119864812e4fa97122fc45
Hoàng Rio <donghoang.nguyen@outlook.com>
2024-04-12 21:03:37 +0700

    Config log level via env

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ SLEEP_TIME = 60
 USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:124.0) Gecko/20100101 Firefox/124.0"
 SERIAL_NUM = env["SERIAL_NUM"]
 LOG_FILE = 'logs/app.log'
-LOG_LEVEL = logging.INFO
+LOG_LEVEL = logging.DEBUG if env["IS_DEBUG"] == "True" else logging.INFO
 LOG_FORMAT = "%(asctime)s %(levelname)s:%(name)s: %(message)s"
 AUDIO_BASE_URL = env["AUDIO_BASE_URL"]
 STATE_FILE = 'grid_connect_state.ini'
