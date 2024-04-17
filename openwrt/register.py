30280f14fd39d8847b4cf7fb8b65010fd3c67b94
Hoàng Rio <donghoang.nguyen@outlook.com>
2024-04-17 16:37:24 +0700

    Change line ending to lf, complete handle device register

--- openwrt/register.py
+++ openwrt/register.py
@@ -1,5 +1,36 @@
 #!/usr/bin/env python
-print("content-type: text/html")
+import json
+import cgi
+from os import path
+
+print("content-type: application/json")
 print("")
-print("<html><head><title>Device notification register</title></head><body>")
-print("<h1>Body text</h1></body></html>")
+DEVICES_FILE = '/mnt/sda1/Programs/grid-watcher/devices.json'
+ret_data = {
+    "is_success": False,
+    "message": "Unknow error",
+    "device_count": 0
+}
+try:
+    form = cgi.FieldStorage()
+    if "token" in form:
+        req_token = form["token"].value  # Get token from post field 'token'
+        if path.exists(DEVICES_FILE):
+            with open(DEVICES_FILE) as fd:
+                devices_json = list(json.loads(fd.read()))
+                if req_token in devices_json:
+                    ret_data["is_success"] = False
+                    ret_data["message"] = "Device already register"
+                else:
+                    with open(DEVICES_FILE, 'w') as fw:
+                        fw.write(json.dumps(devices_json))
+                        devices_json.append(req_token)
+                        ret_data["is_success"] = True
+                        ret_data["message"] = "Device register success"
+                        ret_data["device_count"] = len(devices_json)
+    else:
+        ret_data["is_success"] = False
+        ret_data["message"] = "Missing required parameter 'token'"
+except Exception as e:
+    ret_data["message"] = "Got exception %s".format(e)
+print(json.dumps(ret_data))
