c44249149b337473e0822dafa9b89d4368a22d8e
Hoàng Rio <donghoang.nguyen@outlook.com>
2024-11-20 14:59:17 +0700

    web: inital web viewer

---
+++ web_viewer/__main__.py
@@ -0,0 +1,56 @@
+import asyncio
+from aiohttp import web, WSCloseCode
+import aiohttp
+from os import environ, path
+
+from dotenv import dotenv_values
+
+
+config: dict = {
+    **dotenv_values(".env"),
+    **environ
+}
+
+async def http_handler(request: web.Request):
+    return web.FileResponse("./public/index.html")
+
+
+async def websocket_handler(request):
+    ws = web.WebSocketResponse()
+    await ws.prepare(request)
+
+    async for msg in ws:
+        if msg.type == aiohttp.WSMsgType.TEXT:
+            if msg.data == "close":
+                await ws.close()
+            else:
+                await ws.send_str("some websocket message payload")
+        elif msg.type == aiohttp.WSMsgType.ERROR:
+            print("ws connection closed with exception %s" % ws.exception())
+
+    return ws
+
+
+def create_runner():
+    app = web.Application()
+    app.add_routes([
+        web.get("/",   http_handler),
+        web.get("/ws", websocket_handler),
+        web.static("/", path.join(path.dirname(__file__), "public"))
+    ])
+    return web.AppRunner(app)
+
+
+async def start_server(host="127.0.0.1", port=1337):
+    runner = create_runner()
+    print(f"Start server on {config["HOST"]}:{config["PORT"]}")
+    await runner.setup()
+    site = web.TCPSite(runner, host, port)
+    await site.start()
+
+
+if __name__ == "__main__":
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    loop.run_until_complete(start_server(host=config["HOST"], port=int(config["PORT"])))
+    loop.run_forever()
