f82b65e4e1818e27654656f961ec6d190314ecf6
Hoàng Rio <donghoang.nguyen@outlook.com>
2024-12-31 10:34:11 +0700

    web: update script references and add date range for energy chart

---
+++ web_viewer/public/assets/GeneralValue-C5vx7WW5.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-Bjtz3g86.js";function l({value:r,unit:s,className:n}){return e.jsxs("div",{className:`${n||""} general-value`,children:[e.jsx("strong",{children:r}),s]})}export{l as G};
