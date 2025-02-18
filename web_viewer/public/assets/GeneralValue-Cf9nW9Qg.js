fb0f12f68e7ac095f0be276bed5de057fd0aa79f
Hoàng Rio <donghoang.nguyen@outlook.com>
2025-02-18 11:46:49 +0700

    chore: update dependencies for @vitejs/plugin-react-swc and vite

---
+++ web_viewer/public/assets/GeneralValue-Cf9nW9Qg.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-C69RpZ6f.js";function l({value:r,unit:s,className:n}){return e.jsxs("div",{className:`${n||""} general-value`,children:[e.jsx("strong",{children:r}),s]})}export{l as G};
