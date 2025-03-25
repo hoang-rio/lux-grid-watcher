783757a836764e339f7f7032288f16f5a04c841e
Hoàng Rio <donghoang.nguyen@outlook.com>
2025-03-25 14:31:29 +0700

    feat: add language selection feature to footer component with localization support

--- web_viewer/build/assets/GeneralValue-3-OSqrXY.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-C5GZMVmA.js";function l({value:r,unit:s,className:n}){return e.jsxs("div",{className:`${n||""} general-value`,children:[e.jsx("strong",{children:r}),s]})}export{l as G};
--- web_viewer/fe_src/src/components/Footer.tsx
+++ web_viewer/fe_src/src/components/Footer.tsx
@@ -1,20 +1,61 @@
 import { memo } from "react";
 import { useTranslation } from "react-i18next";
+import "./Footer.css";
 
 function Footer() {
-  const { t } = useTranslation();
+  const { t, i18n } = useTranslation();
+
+  const languages = [
+    { code: "vi", name: "Tiếng Việt" },
+    { code: "en", name: "English" },
+  ];
+
+  const handleLanguageChange = (lang: string) => {
+    i18n.changeLanguage(lang);
+  };
 
   return (
     <div className="card text-center footer">
       &copy; 2024{" "}
-      <a href="https://hoangnguyendong.dev" target="_blank">
+      <a
+        href="https://hoangnguyendong.dev"
+        target="_blank"
+        className="footer-link"
+      >
         Hoàng Rio
       </a>
-      <br />
-      {t('openSourceAt')}{" "}
-      <a href="https://github.com/hoang-rio/lux-grid-watcher" target="_blank">
+      <div className="footer-spacing" />
+      {t("openSourceAt")}{" "}
+      <a
+        href="https://github.com/hoang-rio/lux-grid-watcher"
+        target="_blank"
+        className="footer-link"
+      >
         Github
       </a>
+      <div className="footer-spacing" />
+      <div className="footer-lang-container">
+        {languages.map((lang, index) => (
+          <>
+            <a
+              href="#"
+              key={lang.code}
+              onClick={(e) => {
+                e.preventDefault();
+                handleLanguageChange(lang.code);
+              }}
+              className={`footer-lang-link ${
+                i18n.language === lang.code ? "active" : ""
+              }`}
+            >
+              {lang.name}
+            </a>
+            {index < languages.length - 1 && (
+              <span className="footer-lang-separator">|</span>
+            )}
+          </>
+        ))}
+      </div>
     </div>
   );
 }
