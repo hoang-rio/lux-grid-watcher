fb22337d5a49286bee2730f4b8427fc60ac6094e
Hoàng Rio <donghoang.nguyen@outlook.com>
2025-04-10 13:05:08 +0700

    style: improve notification popover layout and enhance close button font size

--- web_viewer/build/assets/react-apexcharts.min-CaXuj3eR.js
+++ web_viewer/build/assets/react-apexcharts.min-CaXuj3eR.js
@@ -1,4 +1,4 @@
-import{g as as,a as ss,b as rs}from"./index-DFfg3Hw9.js";var Ft={};/*!
+import{g as as,a as ss,b as rs}from"./index-D11lsB9n.js";var Ft={};/*!
  * ApexCharts v4.5.0
  * (c) 2018-2025 ApexCharts
  * Released under the MIT License.
