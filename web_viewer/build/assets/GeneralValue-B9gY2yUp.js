a2b03de199f432e6612ccbb32dd90fb9c1675f13
Hoàng Rio <donghoang.nguyen@outlook.com>
2025-04-10 12:53:44 +0700

    build: fe build

---
+++ web_viewer/build/assets/GeneralValue-B9gY2yUp.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-DFfg3Hw9.js";function l({value:r,unit:s,className:n}){return e.jsxs("div",{className:`${n||""} general-value`,children:[e.jsx("strong",{children:r}),s]})}export{l as G};
