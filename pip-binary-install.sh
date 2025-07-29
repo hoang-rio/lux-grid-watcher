055fce4c67d25599a7aade3f92ed1d7b33f3bd05
Hoàng Rio <donghoang.nguyen@outlook.com>
2025-07-29 15:14:42 +0700

    feat: use venv to run python app

--- pip-binary-install.sh
+++ pip-binary-install.sh
@@ -1,4 +1,4 @@
 echo "Starting to install requirements.txt..."
+source venv/bin/activate
+pip install --upgrade pip
 pip install --only-binary :all: --no-build-isolation -r requirements.txt
-rm -rf aiohttp/aiohttp/_websocket/reader_c.py
-cp -r aiohttp/aiohttp/_websocket/reader_py.py aiohttp/aiohttp/_websocket/reader_c.py
--- restart-app.sh
+++ restart-app.sh
@@ -1,3 +1,4 @@
+source venv/bin/activate
 PID=$(ps | grep app.py | grep -v grep | awk '{print $1}')
 if [ -z "$PID" ]; then
     echo "app.py is not running."
